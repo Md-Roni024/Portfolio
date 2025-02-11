83b521fcbaeb6a18a3ebef46407e9a9a51938805
Roni Hossain <roni.cse024@gmail.com>
2025-02-11 13:42:18 +0600

    Update Resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/Md-Roni024",
     resume:
-      "https://drive.google.com/file/d/1mhpAbFNDdtAK6jVlGeQBtctsmpw3V6ig/view?usp=sharing",
+      "https://drive.google.com/file/d/1L9zsT-28npW64BbW5TbO2RRU_3xBm4KE/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/roni-hossain-z24/",
     codeforces: "https://codeforces.com/profile/Roni24",
     leetcode: "https://leetcode.com/Roni_024/",
